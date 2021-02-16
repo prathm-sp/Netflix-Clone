cbee2bf2a099d9114302992e2c003e4d7fd7b5ff
prathm-sp <prathmeshkulkarni98882@gmail.com>
2021-02-16 22:49:19 +0530

    done

---
+++ src/request.js
@@ -0,0 +1,14 @@
+const API_KEY = "de88566cf29ef654e21394ab0906ad7e";
+
+const request = {
+  fetchTrending: `/trending/all/day?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOrignal: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_generes=28`,
+  fetchUpcomingMovies: `/movie/upcoming?api_key=${API_KEY}&language=en-US`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_generes=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_generes=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_generes=10749`,
+  fetchDocumentaryMovies: `/discover/movie?api_key=${API_KEY}&with_generes=99`,
+};
+export default request;
