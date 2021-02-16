8e5e8af672a8306981d3a9b7252cc0aea8eea324
prathm-sp <prathmeshkulkarni98882@gmail.com>
2021-02-16 23:22:55 +0530

    done

--- src/Banner.js
+++ src/Banner.js
@@ -18,7 +18,9 @@ function Banner() {
     }
     fetchData();
   }, []);
-  console.log(movies);
+  function truncate(str, n) {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  }
   return (
     <header
       className="banner"
@@ -29,13 +31,18 @@ function Banner() {
       }}
     >
       <div className="banner_content">
-        <h1>{movies?.title || movies?.name || movies?.original_name}</h1>
+        <h1 className="banner_title">
+          {movies?.title || movies?.name || movies?.original_name}
+        </h1>
         <div className="banner_buttons">
           <button className="banner_button">Play</button>
           <button className="banner_button">My List</button>
         </div>
-        <h1 className="banner_description">{movies?.overview}</h1>
+        <h1 className="banner_description">
+          {truncate(movies?.overview, 150)}
+        </h1>
       </div>
+      <div className="banner--fadeBottom"></div>
     </header>
   );
 }
