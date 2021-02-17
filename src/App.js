eba7cf6ad3b706a2815bd2dfbc6ecdb5657baf3d
prathm-sp <prathmeshkulkarni98882@gmail.com>
2021-02-17 18:00:31 +0530

    done

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import "./App.css";
 import request from "./request";
 import Row from "./Row";
 import Banner from "./Banner";
+import Nav from "./Nav";
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <Banner />
       <Row
         title="Netflix Orignal Movies"
