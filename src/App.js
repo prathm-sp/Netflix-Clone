ac678f0bcf53d0aa8acbace88d0ecc6961df45bb
prathm-sp <prathmeshkulkarni98882@gmail.com>
2021-02-18 18:02:04 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,27 +1,21 @@
 import "./App.css";
-import request from "./request";
-import Row from "./Row";
-import Banner from "./Banner";
-import Nav from "./Nav";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import HomeScreen from "./HomeScreen";
+import LoginScreen from "./LoginScreen";
 
 function App() {
   return (
     <div className="App">
-      <Nav />
-      <Banner />
-      <Row
-        title="Netflix Orignal Movies"
-        URL={request.fetchNetflixOrignal}
-        isLarge
-      />
-      <Row title="Trending Movies" URL={request.fetchTrending} />
-      <Row title="Action Movies" URL={request.fetchActionMovies} />
-      <Row title="Upcoming Movies" URL={request.fetchUpcomingMovies} />
-      <Row title="Comedy Movies" URL={request.fetchComedyMovies} />
-      <Row title="Documentary Movies" URL={request.fetchDocumentaryMovies} />
-      <Row title="Horror Movies" URL={request.fetchHorrorMovies} />
-      <Row title="Romance Movies" URL={request.fetchRomanceMovies} />
-      <Row title="Top Rated Movies" URL={request.fetchTopRated} />
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <HomeScreen />
+          </Route>
+          <Route exact path="/Login">
+            <LoginScreen />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
