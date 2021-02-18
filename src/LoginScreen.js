e9181e2f3b1a422991048064b0a642bb5b5b121c
prathm-sp <prathmeshkulkarni98882@gmail.com>
2021-02-18 18:03:33 +0530

    done

---
+++ src/LoginScreen.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./LoginScreen.css";
+function Login() {
+  return (
+    <div className="loginScreen">
+      <img
+        className="loginScreen_logo"
+        src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
+      />
+      <button className="loginScreen_button">Sign In</button>
+      <div className="loginScreen_gradient" />
+      <div className="loginScreen_body">
+        <h1>Unlimited films,TV programmers and more.</h1>
+        <h2>Watch anywhere. Cancel at any time</h2>
+        <h3>
+          Ready to watch? Enter your email to create or restart your membership.
+        </h3>
+
+        <div className="loginScreen_input">
+          <form>
+            <input type="email" placeholder="Email Address" />
+            <button className="loginScreen_getStarted">Get Started</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
