5cd7f72fc8d44003981f2a262a907e94f2526c4c
prathm-sp <prathmeshkulkarni98882@gmail.com>
2021-02-19 18:33:47 +0530

    done

--- src/LoginScreen.js
+++ src/LoginScreen.js
@@ -1,12 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./LoginScreen.css";
 function Login() {
+  console.log("Coming");
   return (
     <div className="loginScreen">
-      <img
-        className="loginScreen_logo"
-        src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
-      />
+      <Link to="/">
+        <img
+          className="loginScreen_logo"
+          src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
+        />
+      </Link>
       <button className="loginScreen_button">Sign In</button>
       <div className="loginScreen_gradient" />
       <div className="loginScreen_body">
