8bdfe65ec129fdb60f8f6438c4411cc2e47161ca
prathm-sp <prathmeshkulkarni98882@gmail.com>
2021-02-19 18:34:25 +0530

    done

--- src/Nav.js
+++ src/Nav.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import "./Nav.css";
 
 function Nav() {
   const [show, handleShow] = useState();
+  console.log("Coming");
   useEffect(() => {
     window.addEventListener("scroll", () => {
       if (window.scrollY > 100) {
@@ -12,21 +14,26 @@ function Nav() {
       }
     });
     return () => {
-      window.removeEventListener("scroll");
+      window.removeEventListener("scroll", () => {
+        return null;
+      });
     };
   }, []);
   return (
     <div className={`nav ${show && "nav_black"}`}>
       <img
         className="nav_logo"
-        src="https://upload.wikimedia.org/wikipedia/commons/0/0f/Logo_Netflix.png"
+        src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
         alt="Netflix Logo"
       />
-      <img
-        className="nav_avatar"
-        src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png"
-        alt="Netflix Logo"
-      />
+      <Link to="login">
+        {/* <img
+          className="nav_avatar"
+          src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png"
+          alt="Netflix Logo"
+        /> */}
+        <button className="loginScreen_button">Sign In</button>
+      </Link>
     </div>
   );
 }
