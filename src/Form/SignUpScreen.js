afeeec51767748b667db6dbe43a8a6064b77c80a
prathm-sp <prathmeshkulkarni98882@gmail.com>
2021-02-21 22:34:37 +0530

    done

---
+++ src/Form/SignUpScreen.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./SignUpScreen.css";
+
+function SignUpScreen() {
+  return <div className="SignUpScreen"></div>;
+}
+
+export default SignUpScreen;
---
+++ src/HomeScreen/Banner.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from "react";
+import axios from "../axios";
+import request from "./request";
+import "./Banner.css";
+
+function Banner() {
+  const [movies, setMovies] = useState([]);
+  console.log("Coming");
+  useEffect(() => {
+    async function fetchData() {
+      const response = await axios.get(request.fetchNetflixOrignal);
+      console.log(response);
+      setMovies(
+        response.data.results[
+          Math.floor(Math.random() * response.data.results.length - 1)
+        ]
+      );
+      console.log(movies);
+    }
+    fetchData();
+  }, []);
+  function truncate(str, n) {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  }
+  return (
+    <header
+      className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movies?.backdrop_path}")`,
+        backgroundPosition: "center center",
+      }}
+    >
+      <div className="banner_content">
+        <h1 className="banner_title">
+          {movies?.title || movies?.name || movies?.original_name}
+        </h1>
+        <div className="banner_buttons">
+          <button className="banner_button">Play</button>
+          <button className="banner_button">My List</button>
+        </div>
+        <h1 className="banner_description">
+          {truncate(movies?.overview, 150)}
+        </h1>
+      </div>
+      <div className="banner--fadeBottom"></div>
+    </header>
+  );
+}
+
+export default Banner;
