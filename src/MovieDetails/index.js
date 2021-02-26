3ba5c583771a275dd629b146ed9cc5e9aa673469
prathm-sp <prathmeshkulkarni98882@gmail.com>
2021-02-26 23:30:59 +0530

    done

--- src/MovieDetails/index.js
+++ src/MovieDetails/index.js
@@ -1,194 +1,177 @@
 import React from "react";
-
+import "./style.css";
 function Index() {
-  console.log("App Started");
   return (
-    <div class="container">
-      <div id="demo" class="carousel slide" data-ride="carousel">
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-            <div class="card">
-              <div class="path">
-                HOME / FACE <a>/ CLEANSERS</a>{" "}
+    <>
+      <div class="movie-card">
+        <div class="container">
+          <a href="#">
+            <img
+              src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/195612/hobbit_cover.jpg"
+              alt="cover"
+              class="cover"
+            />
+          </a>
+
+          <div class="hero">
+            <div class="details">
+              <div class="title1">
+                The Hobbit <span>PG-13</span>
               </div>
-              <div class="row">
-                <div class="col-md-6 text-center align-self-center">
-                  {" "}
-                  <img
-                    class="img-fluid"
-                    src="https://i.imgur.com/7a9V4yw.png"
-                  />{" "}
-                </div>
-                <div class="col-md-6 info">
-                  <div class="row title">
-                    <div class="col">
-                      <h2>Herbalism</h2>
-                    </div>
-                    <div class="col text-right">
-                      <a href="#">
-                        <i class="fa fa-heart-o"></i>
-                      </a>
-                    </div>
-                  </div>
-                  <p>Natural herbal wash</p>{" "}
-                  <span class="fa fa-star checked"></span>{" "}
-                  <span class="fa fa-star checked"></span>{" "}
-                  <span class="fa fa-star checked"></span>{" "}
-                  <span class="fa fa-star checked"></span>{" "}
-                  <span class="fa fa-star-half-full"></span>{" "}
-                  <span id="reviews">1590 Reviews</span>
-                  <div class="row price">
-                    {" "}
-                    <label class="radio">
-                      {" "}
-                      <input
-                        type="radio"
-                        name="size1"
-                        value="small"
-                        checked
-                      />{" "}
-                      <span>
-                        <div class="row">
-                          <big>
-                            <b>1.5 oz.</b>
-                          </big>
-                        </div>
-                        <div class="row">$12.95</div>
-                      </span>{" "}
-                    </label>{" "}
-                    <label class="radio">
-                      {" "}
-                      <input type="radio" name="size1" value="large" />{" "}
-                      <span>
-                        <div class="row">
-                          <big>
-                            <b>4.4 oz.</b>
-                          </big>
-                        </div>
-                        <div class="row">$21.95</div>
-                      </span>{" "}
-                    </label>{" "}
-                  </div>
-                </div>
-              </div>
-              <div class="row lower">
-                <div class="col">
-                  {" "}
-                  <a
-                    class="carousel-control-prev"
-                    href="#demo"
-                    data-slide="prev"
-                  >
-                    <i class="fa fa-arrow-left"></i>
-                  </a>{" "}
-                  <a
-                    class="carousel-control-next"
-                    href="#demo"
-                    data-slide="next"
-                  >
-                    <i class="fa fa-arrow-right"></i>
-                  </a>{" "}
-                </div>
-                <div class="col text-right align-self-center">
-                  {" "}
-                  <button class="btn">Add to cart</button>{" "}
-                </div>
-              </div>
+
+              <div class="title2">The Battle of the Five Armies</div>
+
+              <fieldset class="rating">
+                <input type="radio" id="star5" name="rating" value="5" />
+                <label
+                  class="full"
+                  for="star5"
+                  title="Awesome - 5 stars"
+                ></label>
+                <input
+                  type="radio"
+                  id="star4half"
+                  name="rating"
+                  value="4 and a half"
+                />
+                <label
+                  class="half"
+                  for="star4half"
+                  title="Pretty good - 4.5 stars"
+                ></label>
+                <input
+                  type="radio"
+                  id="star4"
+                  name="rating"
+                  value="4"
+                  checked
+                />
+                <label
+                  class="full"
+                  for="star4"
+                  title="Pretty good - 4 stars"
+                ></label>
+                <input
+                  type="radio"
+                  id="star3half"
+                  name="rating"
+                  value="3 and a half"
+                />
+                <label
+                  class="half"
+                  for="star3half"
+                  title="Meh - 3.5 stars"
+                ></label>
+                <input type="radio" id="star3" name="rating" value="3" />
+                <label class="full" for="star3" title="Meh - 3 stars"></label>
+                <input
+                  type="radio"
+                  id="star2half"
+                  name="rating"
+                  value="2 and a half"
+                />
+                <label
+                  class="half"
+                  for="star2half"
+                  title="Kinda bad - 2.5 stars"
+                ></label>
+                <input type="radio" id="star2" name="rating" value="2" />
+                <label
+                  class="full"
+                  for="star2"
+                  title="Kinda bad - 2 stars"
+                ></label>
+                <input
+                  type="radio"
+                  id="star1half"
+                  name="rating"
+                  value="1 and a half"
+                />
+                <label
+                  class="half"
+                  for="star1half"
+                  title="Meh - 1.5 stars"
+                ></label>
+                <input type="radio" id="star1" name="rating" value="1" />
+                <label
+                  class="full"
+                  for="star1"
+                  title="Sucks big time - 1 star"
+                ></label>
+                <input type="radio" id="starhalf" name="rating" value="half" />
+                <label
+                  class="half"
+                  for="starhalf"
+                  title="Sucks big time - 0.5 stars"
+                ></label>
+              </fieldset>
+
+              <span class="likes">109 likes</span>
             </div>
           </div>
-          <div class="carousel-item">
-            <div class="card">
-              <div class="path">
-                HOME / FACE <a>/ CLEANSERS</a>{" "}
-              </div>
-              <div class="row">
-                <div class="col-md-6 text-center align-self-center">
-                  {" "}
+
+          <div class="description">
+            <div class="column1">
+              <span
+                class="tag"
+                style={{ backgroundColor: "#7206f7", padding: "4px 10px" }}
+              >
+                action
+              </span>
+              <span
+                class="tag"
+                style={{ backgroundColor: "#f70661", padding: "4px 10px" }}
+              >
+                fantasy
+              </span>
+              <span
+                class="tag"
+                style={{
+                  backgroundColor: "rgb(30 179 20)",
+                  padding: "4px 10px",
+                }}
+              >
+                adventure
+              </span>
+            </div>
+
+            <div class="column2">
+              <p>
+                Bilbo Baggins is swept into a quest to reclaim the lost Dwarf
+                Kingdom of Erebor from the fearsome dragon Smaug. Approached out
+                of the blue by the wizard Gandalf the Grey, Bilbo finds himself
+                joining a company of thirteen dwarves led by the legendary
+                warrior, Thorin Oakenshield. Their journey will take them into
+                the Wild; through... <a href="#">read more</a>
+              </p>
+
+              <div class="avatars">
+                <a href="#" data-tooltip="Person 1" data-placement="top">
                   <img
-                    class="img-fluid"
-                    src="https://i.imgur.com/7a9V4yw.png"
-                  />{" "}
-                </div>
-                <div class="col-md-6 info">
-                  <div class="row title">
-                    <div class="col">
-                      <h2>Herbalism 2</h2>
-                    </div>
-                    <div class="col text-right">
-                      <a href="#">
-                        <i class="fa fa-heart-o"></i>
-                      </a>
-                    </div>
-                  </div>
-                  <p>Natural herbal wash pro</p>{" "}
-                  <span class="fa fa-star checked"></span>{" "}
-                  <span class="fa fa-star checked"></span>{" "}
-                  <span class="fa fa-star checked"></span>{" "}
-                  <span class="fa fa-star checked"></span>{" "}
-                  <span class="fa fa-star-half-full"></span>{" "}
-                  <span id="reviews">1590 Reviews</span>
-                  <div class="row price">
-                    {" "}
-                    <label class="radio">
-                      {" "}
-                      <input type="radio" name="size2" value="small" />{" "}
-                      <span>
-                        <div class="row">
-                          <big>
-                            <b>13.5 oz.</b>
-                          </big>
-                        </div>
-                        <div class="row">$36.95</div>
-                      </span>{" "}
-                    </label>{" "}
-                    <label class="radio">
-                      {" "}
-                      <input
-                        type="radio"
-                        name="size2"
-                        value="large"
-                        checked
-                      />{" "}
-                      <span>
-                        <div class="row">
-                          <big>
-                            <b>18.4 oz.</b>
-                          </big>
-                        </div>
-                        <div class="row">$61.95</div>
-                      </span>{" "}
-                    </label>{" "}
-                  </div>
-                </div>
-              </div>
-              <div class="row lower">
-                <div class="col">
-                  {" "}
-                  <a
-                    class="carousel-control-prev"
-                    href="#demo"
-                    data-slide="prev"
-                  >
-                    <i class="fa fa-arrow-left"></i>
-                  </a>{" "}
-                  <a
-                    class="carousel-control-next"
-                    href="#demo"
-                    data-slide="next"
-                  >
-                    <i class="fa fa-arrow-right"></i>
-                  </a>{" "}
-                </div>
-                <div class="col text-right align-self-center">
-                  {" "}
-                  <button class="btn">Add to cart</button>{" "}
-                </div>
+                    src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/195612/hobbit_avatar1.png"
+                    alt="avatar1"
+                  />
+                </a>
+
+                <a href="#" data-tooltip="Person 2" data-placement="top">
+                  <img
+                    src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/195612/hobbit_avatar2.png"
+                    alt="avatar2"
+                  />
+                </a>
+
+                <a href="#" data-tooltip="Person 3" data-placement="top">
+                  <img
+                    src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/195612/hobbit_avatar3.png"
+                    alt="avatar3"
+                  />
+                </a>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
