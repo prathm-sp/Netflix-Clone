a9f5a5684e1921bbe41a071a2e50af824a00e936
prathmesh <prathmeshkulkarni98882@gmail.com>
2021-02-28 23:56:42 +0530

    done

--- src/Form/SignUpScreen.js
+++ src/Form/SignUpScreen.js
@@ -1,9 +1,28 @@
 import React from "react";
 import "./SignUpScreen.css";
-
+import { Link } from "react-router-dom";
+import Nav from "../Navbar/Nav";
 function SignUpScreen() {
   console.log("App Started");
-  return <div className="SignUpScreen"></div>;
+  return (
+    <div className="loginScreen">
+      <Nav />
+      <div className="loginScreen_gradient" />
+      <div className="loginScreen_body">
+        <div className="loginScreen_input">
+          <div className="main_register">
+            <form className="registerForm">
+              <input type="text" placeholder="Enter Email Address" />
+              <input type="password" placeholder="Enter Password" />
+              <button>Login</button>
+
+              <Link to="/Login">New Here? Register</Link>
+            </form>
+          </div>{" "}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default SignUpScreen;
