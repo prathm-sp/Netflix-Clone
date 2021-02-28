2256bb1b96d65b2043a66ad856e41c88a18fd30e
prathmesh <prathmeshkulkarni98882@gmail.com>
2021-02-28 23:56:17 +0530

    done

--- src/App.js
+++ src/App.js
@@ -2,24 +2,31 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import HomeScreen from "./HomeScreen/HomeScreen";
 import LoginScreen from "./Form/LoginScreen";
+import RegisterScreen from "./Form/SignUpScreen";
 import MovieDetails from "./MovieDetails";
+import axios from "./axios";
+import { useState } from "react";
 
 function App() {
   console.log("App Started");
   console.log("Coming");
-  console.log("App Started");
+  var [movieDetails, setMovieDetails] = useState();
+  console.log("movieDetails = ", movieDetails);
   return (
     <div className="App">
       <Router>
         <Switch>
           <Route exact path="/">
-            <HomeScreen />
+            <HomeScreen setMovieDetails={setMovieDetails} />
           </Route>
           <Route exact path="/Login">
             <LoginScreen />
           </Route>
+          <Route exact path="/Register">
+            <RegisterScreen />
+          </Route>
           <Route exact path="/MovieDetails">
-            <MovieDetails />
+            <MovieDetails movieDetails={movieDetails} />
           </Route>
         </Switch>
       </Router>
