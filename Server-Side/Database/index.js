e79d97d442d860dd0c7276dc7370aa7164b56cf4
prathmesh <prathmeshkulkarni98882@gmail.com>
2021-03-11 16:58:40 +0530

    done

---
+++ Server-Side/Database/index.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+mongoose
+  .connect(process.env.MONGODB_URL, {
+    dbName: process.env.DB_NAME,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("MongoDB connected");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+mongoose.connection.on("connected", () => {
+  console.log("Mongoose Connected to DB");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log(err.message);
+});
+
+mongoose.connection.on("disconnected", () => {
+  console.log("Mongoose connection is disconnected");
+});
+
+process.on("SIGINT", async () => {
+  await mongoose.connection.close();
+  process.exit(0);
+});
