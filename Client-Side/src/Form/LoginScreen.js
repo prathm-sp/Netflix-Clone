be069fde403c146ccf496038c220aa85ff21e925
prathmesh <prathmeshkulkarni98882@gmail.com>
2021-03-24 18:44:38 +0530

    done

--- Client-Side/src/Form/LoginScreen.js
+++ Client-Side/src/Form/LoginScreen.js
@@ -1,27 +1,27 @@
-import React from "react";
-import Nav from "../Navbar/Nav";
-import "./LoginScreen.css";
-function Login() {
-  return (
-    <div className="loginScreen">
-      <Nav />
-      <div className="loginScreen_gradient" />
-      <div className="loginScreen_body">
-        <h1>Unlimited films,TV programmers and more.</h1>
-        <h2>Watch anywhere. Cancel at any time</h2>
-        <h3>
-          Ready to watch? Enter your email to create or restart your membership.
-        </h3>
+// import React from "react";
+// import Nav from "../Navbar/Nav";
+// import "./LoginScreen.css";
+// function Login() {
+//   return (
+//     <div className="loginScreen">
+//       <Nav />
+//       <div className="loginScreen_gradient" />
+//       <div className="loginScreen_body">
+//         <h1>Unlimited films,TV programmers and more.</h1>
+//         <h2>Watch anywhere. Cancel at any time</h2>
+//         <h3>
+//           Ready to watch? Enter your email to create or restart your membership.
+//         </h3>
 
-        <div className="loginScreen_input">
-          <form>
-            <input type="email" placeholder="Email Address" />
-            <button className="loginScreen_getStarted">Get Started</button>
-          </form>
-        </div>
-      </div>
-    </div>
-  );
-}
+//         <div className="loginScreen_input">
+//           <form>
+//             <input type="email" placeholder="Email Address" />
+//             <button className="loginScreen_getStarted">Get Started</button>
+//           </form>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
 
-export default Login;
+// export default Login;
