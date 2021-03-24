d911c820390755f024f42fdf25e7a55566bd03e7
prathmesh <prathmeshkulkarni98882@gmail.com>
2021-03-24 18:46:02 +0530

    done

--- Client-Side/src/Movies/MovieScreen.js
+++ Client-Side/src/Movies/MovieScreen.js
@@ -1,10 +1,28 @@
 import React, { useState, useEffect } from "react";
 import Row from "./Row";
 import Banner from "./Banner";
-import Nav from "../Navbar/Nav";
+import Nav from "../Header/Nav";
 import axios from "../axios";
+import { Link } from "react-router-dom";
 
 function HomeScreen(props) {
+  const [show, handleShow] = useState();
+  console.log("App Started");
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else {
+        handleShow(false);
+      }
+    });
+    return () => {
+      window.removeEventListener("scroll", () => {
+        return null;
+      });
+    };
+  }, []);
+
   const [movies, setMovies] = useState();
   useEffect(() => {
     axios
@@ -20,7 +38,31 @@ function HomeScreen(props) {
 
   return (
     <div style={{ background: "#111" }}>
-      <Nav />
+      <div
+        className={`nav ${show && "nav_black"}`}
+        style={
+          props?.position
+            ? { position: `${props?.position}` }
+            : { position: "fixed" }
+        }
+      >
+        <Link to="/">
+          {" "}
+          <img
+            className="nav_logo"
+            src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
+            alt="Netflix Logo"
+          />
+        </Link>
+        <Link to="/login">
+          {/* <img
+          className="nav_avatar"
+          src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png"
+          alt="Netflix Logo"
+        /> */}
+          <button className="loginScreen_button">Sign In</button>
+        </Link>
+      </div>
       <Banner Data={movies} />
       {movies?.map?.((item) => (
         <Row title={item?.CategoryName} Data={item?.Movies} isLarge />
